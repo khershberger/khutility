45c2f167d12b4d63caf9dba172e3da0f15137c36
Kyle Hershberger <kyle.hershberger@gmail.com>
2021-09-16 15:38:19 -0400

    Fix problems with setup.py

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ except ImportError:
     from distutils.core import setup
 
 setup(name = 'khutility',
-      description = 'Universal Utility Functions',
-      authon = 'Kyle Hershberger',
-      version = '0.1.1',
-      packages = ['pyate'],
-      install_requires = [
-          'matplotlib',
-		  'numpy',
-          'pandas',
-          'pyqt5',
-          'scipy'
-          ]
-      )
+    description = 'Utility Functions by and for Kyle',
+    authon = 'Kyle Hershberger',
+    version = '0.1.1',
+    packages = ['khutility'],
+    install_requires = [
+        'matplotlib',
+        'numpy',
+        'pandas',
+        'pyqt5',
+        'scipy'
+        ]
+    )
